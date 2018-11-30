29b4188ce9d84e0a0d6933208afd03b190e47a28
LazzerDrummer <lazerdrummer@gmail.com>
2018-12-01 00:33:25 +0200

    You can now have as many squares as you like. Board outline. Frozen Lake

--- interface.py
+++ interface.py
@@ -8,36 +8,49 @@ pygame.init()
 black = (0, 0, 0)
 white = (255, 255, 255)
 grey = (128, 128, 128)
+blue = (128, 212, 255)
 
 
 class Board:
 
-    def __init__(self, player, side):
-        self.spaces = [0]*9
+    def __init__(self, player, side, tiles_number):
+        self.spaces = [0]*pow(tiles_number, 2)
         self.player = player
         self.side = side
-        self.division = side/3
+        self.tiles_number = tiles_number
+        self.division = side/tiles_number
         self.size = [side, side]
         self.window = pygame.display.set_mode(self.size)
-        pygame.display.set_caption('Tic Tac Toe')
-        self.window.fill(grey)
+        pygame.display.set_caption('Frozen Lake')
+        self.window.fill(blue)
 
-    def show_board(self):
-        pygame.draw.line(self.window, black, [self.division, 0], [
-                         self.division, self.side], 1)
-        pygame.draw.line(self.window, black, [self.division*2, 0], [
-                         self.division*2, self.side], 1)
-        pygame.draw.line(self.window, black, [0, self.division], [
-                         self.side, self.division], 1)
-        pygame.draw.line(self.window, black, [0, self.division*2], [
-                         self.side, self.division*2], 1)
+    def draw_board(self, line_width):
+        start_correction = 1
+        end_correction = 2
+        if line_width == 1:
+            start_correction = 0
+            end_correction = 0
+        elif line_width == 2:
+            start_correction = 0
+            end_correction = 1
+        elif line_width % 2 == 0:
+            start_correction = 1
+            end_correction = 3
+        rectangle = [start_correction, start_correction, self.side -
+                     end_correction, self.side - end_correction]
+        pygame.draw.rect(self.window, black, rectangle, line_width)
+        for i in range(1, self.tiles_number):
+            pygame.draw.line(self.window, black, [self.division*i, 0], [
+                self.division*i, self.side], line_width)
+            pygame.draw.line(self.window, black, [0, self.division*i], [
+                self.side, self.division*i], line_width)
 
 
 def main():
     os.environ['SDL_VIDEO_CENTERED'] = '1'
 
-    frames = 60
-    board = Board(0, 500)
+    # frames = 60
+    board = Board(0, 500, 3)
 
     done = False
     clock = pygame.time.Clock()
@@ -48,8 +61,7 @@ def main():
             if event.type == pygame.QUIT:
                 done = True
 
-        board.show_board()
-        # window.fill(grey)
+        board.draw_board(3)
         pygame.display.flip()
 
     pygame.quit()
